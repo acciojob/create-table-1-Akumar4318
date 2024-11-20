f8a7234dac8ffebba2760cc3d1856c99f1d2d6df
Abhishek Kumar <127936615+Akumar4318@users.noreply.github.com>
2024-11-21 01:01:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ function insert_Row() {
     let newcell1=document.createElement('td')
     let newcell2=document.createElement('td')
 
-    newcell1.innerHTML="New Cell1"
-    newcell2.innerHTML="New Cell2"
+    newcell1.innerHTML="a"
+    newcell2.innerHTML="b"
 
     newrow.appendChild(newcell1)
     newrow.appendChild(newcell2)
